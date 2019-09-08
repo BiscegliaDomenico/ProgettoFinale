778544b477bac2c8a9249cbe662963e34b2bedeb
BiscegliaDomenico <bisceglia.domenico@iisgalvanimi.edu.it>
2019-09-08 13:15:49 +0000

    decimo

--- progetto-finale/src/app/object-list/object-list.component.ts
+++ progetto-finale/src/app/object-list/object-list.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {Utente} from '../utente';
 @Component({
   selector: 'app-object-list',
   templateUrl: './object-list.component.html',
   styleUrls: ['./object-list.component.css']
 })
 export class ObjectListComponent implements OnInit {
-
+  @Input() lista;
   constructor() { }
 
   ngOnInit() {
